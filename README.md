2943b8df1fe5211736e72b0af55d85f54c3d9509
Sandros94 <sandro.circi@digitoolmedia.com>
2025-01-08 21:32:14 +0100

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nuxt Reactive SSE
+
+A simple example on how to use Server-Sent Events (SSE) to update Vue components in real-time.
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/guide/directory-structure/nuxt.config#nuxt-config-file
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/ui-pro',
+    '@nuxtjs/mdc',
+    '@sandros94/lab',
+  ],
+
+  devtools: { enabled: true },
+
+  css: [
+    'assets/css/main.css',
+  ],
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+
+  lab: {
+    kv: true,
+    zlib: true,
+  },
+})
