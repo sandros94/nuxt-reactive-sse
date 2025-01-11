b06b8e256e66d1bdf735715fb20fc3b1e187d529
Sandros94 <sandro.circi@digitoolmedia.com>
2025-01-11 16:20:00 +0100

    up

---
+++ app/components/burger.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { burger } = useMultiState<{
+  burger: string
+}>(['burger'])
+</script>
+
+<template>
+  <p>{{ burger || 'No Burger available' }}</p>
+</template>
---
+++ app/components/pizza.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { pizza } = useMultiState<{
+  pizza: string
+}>(['pizza'])
+</script>
+
+<template>
+  <p>{{ pizza || 'No Pizza available' }}</p>
+</template>
